0fcc28203a6dbed7323b12772e71cc04002f1d32
Jun Matsushita <jun@iilab.org>
2015-12-06 19:14:29 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 var Matcher = require('minimatch').Minimatch;
 
 /**
- * Sets the given metadata on the file object
- *
- * @param file {Object}
- * @param metadata {Object}
- * @private
- */
-function setMetadata(file, rule) {
-    Object.keys(rule.metadata).forEach(function (key) {
-        if (rule.preserve && key in file) {
-            return;
-        }
-        file[key] = rule.metadata[key];
-    });
-}
-
-/**
  * Sets some metadata on each file depending a pattern
  *
  * @param rules {Array} array of rules to set the metadata, each item of
@@ -32,8 +16,6 @@ module.exports = function (rules) {
     rules.forEach(function (rule) {
         matchers.push({
             matcher: new Matcher(rule.pattern),
-            metadata: rule.metadata,
-            preserve: rule.preserve,
         });
     });
 
@@ -43,7 +25,7 @@ module.exports = function (rules) {
 
             matchers.forEach(function (rule) {
                 if ( rule.matcher.match(file) ) {
-                    setMetadata(fileObject, rule);
+                    delete files[file]
                 }
             });
         });
